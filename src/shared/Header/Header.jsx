038b0aa5674436441836bded8fe8dfba5fae1a4a
Mahin01 <Ashmahin4@gmail.com>
2023-05-03 19:58:19 +0600

    Header Navigation Menu Created

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () => {
     return (
-        <div>
-            This is Header
-        </div>
+        <Navbar collapseOnSelect expand="lg">
+            <Container>
+              <Navbar.Brand className='site-title' to="/">Cooking Lights</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="m-auto menu">
+                  <Nav.Link>
+                    <Link className='text-decoration-none menu-item' to="/">Home</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link className='text-decoration-none menu-item' to="/stat">Blog</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link className='text-decoration-none menu-item' to="/applied-jobs">Contact</Link>
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                <Button className="login-btn" variant="primary">Login</Button>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
     );
 };
 
