93142904d235f5acf4d0e8c0a76abc89bcf41306
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 02:52:32 +0600

    Footer layout created

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -2,9 +2,52 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            This is Footer
-        </div>
+    <footer className="page-footer font-small blue pt-4">
+            <div className="container-fluid text-md-left text-sm-center">
+                <div className="row">
+                    <div className="col-md-3 mt-md-0 mt-3 text-md-left">
+                    <h5 className="font-weight-bold">Cooking Lights</h5>
+                        <p>Enjoy the best cuisine experience from our senior & experienced chefs made delicious items.</p>
+                    </div>
+
+                    <hr className="clearfix w-100 d-md-none pb-0"/>
+
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-copyright text-center py-3">Copyright © Cooking Lights 2023
+            </div>
+
+    </footer>
+
     );
 };
 
