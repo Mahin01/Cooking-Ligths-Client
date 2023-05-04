b3ca8429ef6cd55d6c1badae483e1330ab3d2e1a
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 16:51:40 +0600

    Layout displaying error on chefs info fixed

--- src/views/ChefsInfo/ChefsInfo.jsx
+++ src/views/ChefsInfo/ChefsInfo.jsx
@@ -1,41 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
-const ChefsInfo = () => {
-    const [chefsData, setChefsData] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5000/chefs-data')
-        .then(res => res.json())
-        .then(data => setChefsData(data))
-        .catch(error => console.error(error))
-    }, [])
+const ChefsInfo = ({singleChefData}) => {
+    const {id, chefPictureUrl, chefName, numRecipes, numLikes, yearsOfExperience} = singleChefData;
     return (
-        <div>
-                {   chefsData.map(singleChefData => <div className='container'
-                    key = {singleChefData.id}
-                >
-                    <Col xs={12} md={4}>
-                        <Card>
-                         <Card.Img variant="top" src={singleChefData.chefPictureUrl} />
-                            <Card.Body>
-                                <Card.Title>{singleChefData.chefName}</Card.Title>
-                                <Card.Text>
-                                Total Recipes: {singleChefData.numRecipes}
-                                </Card.Text>
-                                <Card.Text>
-                                Experience: {singleChefData.yearsOfExperience}
-                                </Card.Text>
-                                <Card.Text>
-                                Total Likes: {singleChefData.numLikes}
-                                </Card.Text>
-                                <Button variant="primary">View Recipes</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    </div>
-                    
-                )}
-        </div>
+            <Col xs={12} md={4} className='mb-3'>
+                <Card>
+                    <Card.Img variant="top" src={chefPictureUrl} />
+                    <Card.Body>
+                        <Card.Title>{chefName}</Card.Title>
+                        <Card.Text>
+                        Total Recipes: {numRecipes}
+                        </Card.Text>
+                        <Card.Text>
+                        Experience: {yearsOfExperience}
+                        </Card.Text>
+                        <Card.Text>
+                        Total Likes: {numLikes}
+                        </Card.Text>
+                        <Button variant="primary">View Recipes</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
     );
 };
 
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner';
 import ChefsInfo from '../ChefsInfo/ChefsInfo';
 import { Container, Row } from 'react-bootstrap';
 
 const Home = () => {
+    const [chefsData, setChefsData] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/chefs-data')
+        .then(res => res.json())
+        .then(data => setChefsData(data))
+        .catch(error => console.error(error))
+    }, [])
     return (
         <div>
             <Banner></Banner>
             <Container className='my-5'>
                 <h2 className='text-center mb-5'>Meet Our Chefs</h2>
              <Row>
-                <ChefsInfo></ChefsInfo>
+             {   chefsData.map(singleChefData => <ChefsInfo    className='container'
+             key = {singleChefData.id}
+             singleChefData = {singleChefData}
+            >
+            </ChefsInfo>
+            )}
              </Row>
             </Container>
         </div>
