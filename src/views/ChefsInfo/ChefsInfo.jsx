4dcea513e04efa9a6942325cee3830609c9d6459
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 15:40:38 +0600

    Chefs Info Layout Created

--- src/views/ChefsInfo/ChefsInfo.jsx
+++ src/views/ChefsInfo/ChefsInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const ChefsInfo = () => {
     useEffect(() => {
@@ -10,7 +10,24 @@ const ChefsInfo = () => {
     }, [])
     return (
         <div>
-            
+            <Container className='my-5'>
+                <h2 className='text-center mb-5'>Meet Our Chefs</h2>
+                <Row>
+                    <Col md={4}>
+                        <Card style={{ width: '18rem' }}>
+                         <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
