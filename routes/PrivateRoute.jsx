b0407ff163b92f2befc14f16d7b0f4d25f12e190
Mahin01 <Ashmahin4@gmail.com>
2023-05-04 21:47:21 +0600

    Private Route Configured

---
+++ routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProviders';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+    const {user, loading} = useContext(AuthContext);
+
+    if(loading){
+        return <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+        <div className="progress-bar bg-success" style="width: 25%"></div>
+      </div>
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate to="/login" replace={true} ></Navigate>;
+};
+
+export default PrivateRoute;
