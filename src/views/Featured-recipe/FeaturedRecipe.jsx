a0ecf8975205a83323e52fc4f1662ee8e1d81d6a
Mahin01 <Ashmahin4@gmail.com>
2023-05-05 20:04:52 +0600

    Featured recipe Section Style fixed

--- src/views/Featured-recipe/FeaturedRecipe.jsx
+++ src/views/Featured-recipe/FeaturedRecipe.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import "./FeaturedRecipe.css";
 
 const FeaturedRecipe = () => {
     return (
         <Container className='my-5'>
-            <h2 className='text-center mb-5'>Featured Recipe</h2>
+            <h2 className='text-center mb-3'>Featured Recipe</h2>
+            <p className='w-75 text-center m-auto mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae obcaecati earum expedita totam fugiat autem itaque quibusdam veritatis amet odit ullam laborum non iusto dolores dolorem.</p>
             <Row>
                 <Col xs={12} md={4}>
                 <Card>
                     <Card.Img style={{ height:'300px' }} variant="top" src="Lahmacun.jpg" />
                     <Card.Body>
-                        <Card.Title>Lamhacun</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <Card.Title className='title'>Lamhacun</Card.Title>
+                        <Card.Text className="recipe-info">
+                        Lahmacun is a traditional dish from the Middle East and Mediterranean regions. It consists of a thin and crispy dough topped with a mixture of ground meat (usually lamb or beef), tomatoes, onions, parsley, and a variety of spices such as paprika, cumin, and cinnamon.
                         </Card.Text>
+                        <div className="rating mb-2">
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9734;</span>
+                        </div>
+
+                        <Button className='read-more'>Read More</Button>
                     </Card.Body>
                 </Card>
                 </Col>
@@ -22,11 +32,18 @@ const FeaturedRecipe = () => {
                 <Card>
                     <Card.Img style={{ height:'300px' }} variant="top" src="tavuk-sis.jpg" />
                     <Card.Body>
-                        <Card.Title>Tavuk Sis</Card.Title>
+                        <Card.Title className='title'>Tavuk Sis</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Tavuk şiş is a popular Turkish kebab dish made with marinated chicken pieces skewered and grilled over a charcoal fire. The chicken is usually marinated in a mixture of olive oil, lemon juice, garlic, and a variety of spices such as paprika, cumin, and oregano.
                         </Card.Text>
+                        <div className="rating mb-2">
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9734;</span>
+                        </div>
+                        <Button className='read-more'>Read More</Button>
                     </Card.Body>
                 </Card>
                 </Col>
@@ -34,11 +51,18 @@ const FeaturedRecipe = () => {
                 <Card>
                     <Card.Img style={{ height:'300px' }} variant="top" src="baklava.jpg" />
                     <Card.Body>
-                        <Card.Title>Baklava</Card.Title>
+                        <Card.Title className='title'>Baklava</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Baklava is a sweet dessert that is popular in Middle Eastern, Mediterranean, and Balkan cuisine. It is made of layers of phyllo pastry that are filled with chopped nuts, such as walnuts or pistachios, and sweetened with a syrup made of sugar, honey, and lemon juice.
                         </Card.Text>
+                        <div className="rating mb-2">
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9733;</span>
+                            <span className="star">&#9734;</span>
+                        </div>
+                    <Button className='read-more'>Read More</Button>
                     </Card.Body>
                 </Card>
                 </Col>
